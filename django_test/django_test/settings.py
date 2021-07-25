807b15b3cf76c710de0d208089a2c6be3aa83e69
Jae Anonas <jae.anonas@gmail.com>
2021-07-25 13:08:12 +0800

    add css to main html

--- django_test/django_test/settings.py
+++ django_test/django_test/settings.py
@@ -117,7 +117,18 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_DIR = os.path.dirname(__file__)
+
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+                    os.path.join(BASE_DIR, 'static'),
+)
