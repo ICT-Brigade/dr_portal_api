77fde25e6391785536413316433a71282f160a07
palanskiheji <hejipalanski@yahoo.com>
2021-08-28 18:15:56 +0800

    refactor: campaign modal responsiveness

--- static/js/campaign_list.js
+++ static/js/campaign_list.js
@@ -13,30 +13,27 @@ function getById(campaign_id) {
         const btnLabel = data.campaign_type && data.campaign_type.includes('volunteer') ? "VOLUNTEER" : "DONATE";
         const has_campaign_tag = Boolean(data.campaign_type && data.campaign_type != "");
         const modal_body = `
-        <div class="container py-0">
-          <div class="row">
-            <div class="col-md-5" id="modal-campaign-img">
-              <img
-                class="portrait"
-                src="${img}"
-                alt="Image Unavailable"
-                id="img_${campaign_id}"
-              >
-            </div>
-            <div class="col-md-7 text-wrap">
-              <div class="d-flex flex-row">
-                <h4 class="campaign-modal-title">
-                  ${data.source_account}
-                </h4>
-                <div class="px-3">
-                  <span class="${has_campaign_tag ? 'px-3 me-auto campaign-modal-tag' : 'd-none'}">${data.campaign_type}</span>
-                </div>
+          <div class="container-fluid py-0">
+            <div class="row">
+              <div class="col-12 col-lg-5 pt-md-3" id="modal-campaign-img">
+                <img class="portrait" src="${img}" alt="Image Unavailable" id="img_${campaign_id}">
               </div>
-              <p>${data.description}</p>
-              <a class="btn btn-primary dr-btn" href="${data.link}" target="_blank">${btnLabel}</a>
+              <div class="col-12 col-lg-7 text-wrap">
+                <div class="d-flex flex-row pt-3 gap-2">
+                  <div class="campaign-modal-title">
+                    <h4>
+                      ${data.source_account}
+                    </h4>
+                  </div>
+                  <div>
+                    <span class="${has_campaign_tag ? 'px-3 me-auto campaign-modal-tag' : 'd-none'}">${data.campaign_type}</span>
+                  </div>
+                </div>
+                <p class="">${data.description}</p>
+                <a class="btn btn-primary dr-btn" href="${data.link}" target="_blank">${btnLabel}</a>
+              </div>
             </div>
-          </div>
-        </div>`;
+          </div>`;
 
         campaign_modal_body.innerHTML = modal_body;
       } catch (error) {
